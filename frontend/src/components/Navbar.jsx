7bc1a79f2af8db62951f6c851dab35313e6f5a75
Md Waqib Sk <rentusk.1970@gmail.com>
2025-03-21 10:14:09 +0530

    fix:navbar bug

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,38 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {jwtDecode} from "jwt-decode";
-import { Menu, X } from "lucide-react"; 
+import { jwtDecode } from "jwt-decode";
+import { Menu, X } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 export default function Navbar() {
- 
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const [user, setUser] = useState(null);
-  const [isOpen,setIsOpen]=useState(null);
-  const handleLogout=()=>{
+  const [isOpen, setIsOpen] = useState(false);
+  const handleLogout = () => {
     setUser(null);
-    localStorage.removeItem("userData")
-    navigate("/")
-    window.location.reload()
-  }
+    localStorage.removeItem("userData");
+    navigate("/");
+    window.location.reload();
+  };
   useEffect(() => {
-    
-    const token=localStorage.getItem("userData")
-    
-    if(token){
-      try{
-        const userData=jwtDecode(token);
+    const token = localStorage.getItem("userData");
+
+    if (token) {
+      try {
+        const userData = jwtDecode(token);
         console.log(userData);
-        setUser(userData)
-      }
-      catch(err){
-        console.error("invalid token")
-        localStorage.removeItem("userData")
+        setUser(userData);
+      } catch (err) {
+        console.error("invalid token");
+        localStorage.removeItem("userData");
         setUser(null);
       }
-
     }
-    
-    
   }, []);
 
   return (
@@ -41,18 +35,27 @@ export default function Navbar() {
         <span className="font-bold text-white text-lg">metaKGP</span>
       </div>
 
-      <button className="min-[430px]:hidden" onClick={
-        ()=>{setIsOpen(!isOpen)}
-      }>
-        {isOpen? <X size={28} />:<Menu size={28}/> }
+      <button
+        className="min-[430px]:hidden"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {isOpen ? <X size={28} /> : <Menu size={28} />}
       </button>
-      <div className={`flex min-[430px]:space-x-8 transition-all duration-500 max-[430px]:space-y-2  max-[430px]:absolute bg-black max-[430px]:w-full max-[430px]:bg-opacity-100 max-[430px]:top-14 max-[430px]:right-0 max-[430px]:${isOpen?"flex-col":"hidden"}`}>
-       { !user && (<Link
-          to="/register"
-          className="hover:text-black hover:bg-[#f5f3f3]  rounded-md cursor-pointer p-2  transition duration-400"
-        >
-          Register
-        </Link>)}
+      <div
+        className={`flex min-[430px]:space-x-8 transition-all duration-500 max-[430px]:space-y-2  max-[430px]:absolute bg-black max-[430px]:w-full max-[430px]:bg-opacity-100 max-[430px]:top-14 max-[430px]:right-0 ${
+          isOpen ? "max-[430px]:flex-col" : "max-[430px]:hidden"
+        }`}
+      >
+        {!user && (
+          <Link
+            to="/register"
+            className="hover:text-black hover:bg-[#f5f3f3]  rounded-md cursor-pointer p-2  transition duration-400"
+          >
+            Register
+          </Link>
+        )}
         {!user && (
           <Link
             to="/signin"
@@ -61,8 +64,8 @@ export default function Navbar() {
             Signin
           </Link>
         )}
-     
-        {(user?.role === "society" || user?.role==="admin" ) && (
+
+        {(user?.role === "society" || user?.role === "admin") && (
           <Link
             to="/add"
             className="hover:text-black hover:bg-[#f5f3f3] rounded-md cursor-pointer p-2 transition duration-400"
@@ -70,7 +73,6 @@ export default function Navbar() {
             Add Event
           </Link>
         )}
-       
 
         <Link
           to="/"
@@ -78,14 +80,13 @@ export default function Navbar() {
         >
           Home
         </Link>
-        {user?.role &&(
-          
-        <Link
-      onClick={handleLogout}
-        className="hover:text-black hover:bg-[#f5f3f3] rounded-md cursor-pointer p-2 transition duration-400"
-      >
-        Logout
-      </Link>
+        {user?.role && (
+          <Link
+            onClick={handleLogout}
+            className="hover:text-black hover:bg-[#f5f3f3] rounded-md cursor-pointer p-2 transition duration-400"
+          >
+            Logout
+          </Link>
         )}
       </div>
     </nav>
